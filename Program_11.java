18532a27566548db9994230e67d9afca7211ff6a
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 22:03:21 +0530

    Add files via upload

---
+++ Program_11.java
@@ -0,0 +1,16 @@
+
+package Assignment1;
+
+
+public class Program_11 {
+    public static void main(String []args)
+    {
+        float r=7.5f;
+        float pi=3.14f;
+        float perimeter,area;
+        perimeter=2*pi*r;
+        area=pi*r*r;
+        System.out.println("perimeter="+perimeter+  "  area="+area);
+    }
+    
+}
