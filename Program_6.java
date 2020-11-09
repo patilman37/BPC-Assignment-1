457767001b223925d2a657605cc75813d37ab675
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 21:56:01 +0530

    Add files via upload

---
+++ Program_6.java
@@ -0,0 +1,17 @@
+
+package Assignment1;
+
+
+public class Program_6 {
+    public static void main(String []args)
+    {
+        int a=125;
+        int b=24;
+        System.out.println("Addition a+b="+(a+b));
+        System.out.println("Substraction a-b="+(a-b));
+        System.out.println(" MUl a*b="+(a*b));
+        System.out.println("Div a/b="+(a/b));
+        System.out.println("Mod a%b="+(a%b));
+    }
+    
+}
