1d10df9e4c1a402f9d7ae24c94abc641153979da
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 22:08:42 +0530

    Add files via upload

---
+++ Program_16.java
@@ -0,0 +1,25 @@
+
+package Assignment1;
+
+public class Program_16 {
+     public static void main(String []args)
+     {
+       int i;
+         for(i=0;i<6;i++)
+         {
+             if(i==1)
+              System.out.println("[| o o |]");
+             if(i==0)
+              System.out.println(" +'''''+ ");
+             if(i==2)
+              System.out.println(" |  ^  | ");
+             if(i==3)
+              System.out.println(" | '_' | ");
+              if(i==4)
+              System.out.println(" +-----+ ");
+             
+         }
+         
+     }
+    
+}
