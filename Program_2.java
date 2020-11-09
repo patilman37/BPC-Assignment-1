9165e761f1ffb4baf3ec116a4f2098c56f5bd25b
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 21:53:27 +0530

    Add files via upload

---
+++ Program_2.java
@@ -0,0 +1,12 @@
+package Assignment1;
+class Program_2
+{
+    public static void main(String[] args)
+    {
+      int a=10;
+      int b=20;
+      int c= a+b;
+      System.out.println("Addition of 10+20="+c);
+    }
+
+}
