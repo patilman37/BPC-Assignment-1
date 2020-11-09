4b934e467b33442b9abb67974c1cc4473f2f5d4c
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 22:08:06 +0530

    Add files via upload

---
+++ Program_15.java
@@ -0,0 +1,15 @@
+
+package Assignment1;
+
+public class Program_15 {
+    public static void main(String []args)
+    {
+        int a=10,b=20,c=0;
+        System.out.println("Before swap a="+a+"b="+b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("After swap a="+a+"b="+b);
+        
+    }
+}
