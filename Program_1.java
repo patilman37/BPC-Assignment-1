26b7456a47e1bf15458666ab3ae1482be21e57b1
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 21:52:42 +0530

    Add files via upload

---
+++ Program_1.java
@@ -0,0 +1,10 @@
+package Assignment1;
+
+public class Program_1
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello");
+        System.out.print("Manish Patil.");
+    }
+}
