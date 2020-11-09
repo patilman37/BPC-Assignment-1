7057fdfc076630f9b53ea5927c5f7b15a89a1a20
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 21:57:05 +0530

    Add files via upload

---
+++ Program_8.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+public class Program_8 {
+    public static void main(String []args)
+    {
+        int i;
+        
+        for(i=0;i<4;i++)
+        {            
+            if(i==0)
+            { System.out.println("   j    a   v      v  a");}
+            if(i==1)
+            { System.out.println("   j   a a   v    v  a a");}
+            if(i==2)
+            { System.out.println("j  j  aaaaa   v  v  aaaaa");}
+            if(i==3)
+            { System.out.println(" jj  a     a    v  a     a");}
+            
+        }
+    }
+}
