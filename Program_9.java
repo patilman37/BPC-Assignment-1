2d1564296f75a4b4dd2b40529861a1cef63dbe5d
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 21:57:42 +0530

    Add files via upload

---
+++ Program_9.java
@@ -0,0 +1,10 @@
+
+package Assignment1;
+
+public class Program_9 {
+    public static void main(String []args)
+    {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+    
+}
