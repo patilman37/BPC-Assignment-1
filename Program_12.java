c0c9853ca917fc1bae8972ede06eee596e0b5055
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 22:04:25 +0530

    Add files via upload

---
+++ Program_12.java
@@ -0,0 +1,16 @@
+
+package Assignment1;
+
+import java.util.Scanner;
+
+public class Program_12 {
+     public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+         int b=sc.nextInt();
+          int c=sc.nextInt();
+          System.out.println("average of A,B,C="+((a+b+c)/3));
+    }
+    
+}
