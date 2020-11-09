9662a91e5e3461af22ecd753027e2bf4b7d16e7b
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 22:05:09 +0530

    Add files via upload

---
+++ Program_13.java
@@ -0,0 +1,14 @@
+
+package Assignment1;
+
+
+public class Program_13 {
+     public static void main(String []args)
+     {
+         double width,height,area,perimeter;
+         width=5.6f;
+         height=8.5f;
+         System.out.println("Area="+(width*height)+"  perimeter="+(2*(width+height)));
+     }
+    
+}
