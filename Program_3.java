ecc353c1e8049a481b274ab4acf9d32f2934aa24
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 21:53:54 +0530

    Add files via upload

---
+++ Program_3.java
@@ -0,0 +1,15 @@
+
+package Assignment1;
+
+
+public class Program_3 
+{
+    public static void main(String[] args)
+    {
+        int a= 50;
+        int b=3;
+        int c= a/b;
+        System.out.println("50/3="+c);
+    }
+    
+}
