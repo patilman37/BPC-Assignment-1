78ac9fe48f407b03a22a7ecf88bdcb8c616e71cd
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-11-09 22:07:39 +0530

    Add files via upload

---
+++ Program_14.java
@@ -0,0 +1,21 @@
+
+package Assignment1;
+
+
+public class Program_14 {
+    public static void main(String []args)
+    {
+        String p1 = "* * * * * * ===================================\n * * * * * ====================================";
+        String p2 = "===============================================";
+        for(int i=0;i<4;i++)
+        {
+            System.out.println(p1);
+        }
+        System.out.println("* * * * * * ===================================");
+        for(int i = 0; i<6;i++)
+        {
+            System.out.println(p2);
+        }
+    }
+    
+}
